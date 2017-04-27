cf14540ecaba945fa72b3e07a5bf44b2105cb2af
Shelby Tersteeg <satersteeg@gmail.com>
2017-04-26 20:28:29 -0800

    Upload Inverse Matrix Script

---
+++ inverseMatrix.R
@@ -0,0 +1,27 @@
+# A function to cache the inverse of a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                y <<- x
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
+
+cacheSolve <- function(x, ...) {
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+}
